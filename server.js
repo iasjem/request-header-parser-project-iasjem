b585b0289b479aff2f7602e63caeebe5217e7406
Glitch (gabby-violin) <none>
2019-02-24 16:28:44 +0000

    👏💵 Checkpoint ./server.js:3244735/211

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+      "ipadress": req.ip,
+      "language": req.language,
+      "software": req.software
+  });
+});
 
 
 // listen for requests :)
